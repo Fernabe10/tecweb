558e9b4a3390d030b1eec7a255d10318be844645
nortegacamacho <nortegacamacho@gmail.com>
2026-08-04 07:32:50 +0000

    variables

---
+++ nati/javascript/code/2_variables.js
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+    <script languaje="JavaScript">
+    var nombre='Juan';
+    var edad=10;
+    var altura=1.92;
+    var casado=false;
+    document.write(nombre);
+    document.write('<br');
+    document.write(altura);
+    document.write('<br>');
+    document.write(casado);
+    </script>
+    </body>
+</html>
