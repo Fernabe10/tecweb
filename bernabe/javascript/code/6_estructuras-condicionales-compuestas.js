53ba551b57580dc7555c072833c0034299b7ddc8
Fernabe10 <157810977+Fernabe10@users.noreply.github.com>
2026-08-04 10:59:39 +0000

    Correccion parseint ejercicio 6

--- bernabe/javascript/code/6_estructuras-condicionales-compuestas.js
+++ bernabe/javascript/code/6_estructuras-condicionales-compuestas.js
@@ -3,6 +3,9 @@ var num2;
 
 num1 = prompt('Introduzca el primer numero:');
 num2 = prompt('Introduzca el segundo numero:');
+num1 = parseInt(num1);
+num2 = parseInt(num2);
+
 
 if(num1>num2){
     document.write('El numero ' + num1 + ' es mayor que ' + num2);
