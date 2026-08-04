a84d97fca34dcf397c1345da5e9aa3ed38e49735
Jose Maria Mercado Barranco <141660868+JoseM3475@users.noreply.github.com>
2026-08-04 11:48:19 +0000

    Ejercicio 7. Modificacion a 7 notas.

--- profe/javascript/code/7_condicionales_anidados.js
+++ profe/javascript/code/7_condicionales_anidados.js
@@ -1,13 +1,21 @@
-var nota1, nota2, nota3;
+var nota1, nota2, nota3, nota4, nota5, nota6, nota7;
 nota1=prompt('Ingrese 1ra. nota:','');
 nota2=prompt('Ingrese 2da. nota:','');
 nota3=prompt('Ingrese 3ra. nota:','');
-//Convertimos los 3 strings en enteros
+nota4=prompt('Ingrese 4ta. nota:','');
+nota5=prompt('Ingrese 5ta. nota:','');
+nota6=prompt('Ingrese 6ta. nota:','');
+nota7=prompt('Ingrese 7ma. nota:','');
+//Convertimos los 7 strings en enteros
 nota1 = parseInt(nota1);
 nota2 = parseInt(nota2);
 nota3 = parseInt(nota3);
+nota4 = parseInt(nota4);
+nota5 = parseInt(nota5);
+nota6 = parseInt(nota6);
+nota7 = parseInt(nota7);
 var pro;
-pro = (nota1 + nota2 + nota3)/3;
+pro = (nota1 + nota2 + nota3 + nota4 + nota5 + nota6 + nota7)/7;
 if(pro>=7)
 {
     document.write('promocionado');
