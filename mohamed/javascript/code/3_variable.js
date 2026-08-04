83b6f19cf18ccbc7638309b46bb591f1f24f8a40
Mohamed Hamdaoui <mohamedfoot12345@gmail.com>
2026-08-04 09:14:55 +0000

    ejercios variable 2

---
+++ mohamed/javascript/code/3_variable.js
@@ -0,0 +1,9 @@
+var nomb;
+var edad;
+nombre=prompt('Ingrese su nombre:','');
+edad=prompt('Ingrese su edad:','');
+document.write('hola');
+document.write(nombre)
+document.write('asi que tienes');
+document.write(edad);
+document.write('años');
