d3ed7751445bda6184279ec4fd340e117ac08b6e
Mohamed Hamdaoui <mohamedfoot12345@gmail.com>
2026-08-04 09:22:06 +0000

    errata

--- mohamed/javascript/code/3_variable.js
+++ mohamed/javascript/code/3_variable.js
@@ -3,7 +3,7 @@ var edad;
 nombre=prompt('Ingrese su nombre:','');
 edad=prompt('Ingrese su edad:','');
 document.write('hola');
-document.write(nombre)
+document.write(nombre);
 document.write('asi que tienes');
 document.write(edad);
 document.write('años');
