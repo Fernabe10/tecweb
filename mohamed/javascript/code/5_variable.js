434184c4997479c6e40f9bbe22efc12e4ec5ed28
Mohamed Hamdaoui <mohamedfoot12345@gmail.com>
2026-08-04 10:34:50 +0000

    errata

--- mohamed/javascript/code/5_variable.js
+++ mohamed/javascript/code/5_variable.js
@@ -4,5 +4,5 @@ nombre=prompt('Ingrese nombre:','');
 nota=promp('Ingrese su nota:','');
 if (nota>=4)
 {
- document.write(nombre+' esta aprobado con un '+nota);   
+ document.write(nombre +' esta aprobado con un '+ nota);   
 }
